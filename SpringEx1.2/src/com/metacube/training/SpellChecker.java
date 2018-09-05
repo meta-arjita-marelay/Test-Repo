69a4ea4d873f5eecaf7b25603076fab19a2c338e
meta-arjita-marelay <41181680+meta-arjita-marelay@users.noreply.github.com>
2018-09-05 19:53:59 +0530

    Add files via upload

---
+++ SpringEx1.2/src/com/metacube/training/SpellChecker.java
@@ -0,0 +1,30 @@
+package com.metacube.training;
+
+/**
+ * Copyright (c) 2018 Metacube.com. All rights reserved.
+ * This is the Spell Checker class.
+ * @author Arjita
+ *
+ */
+public class SpellChecker {
+
+	private boolean status;
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void checkStatus() {
+		if(getStatus())
+		{
+			System.out.println("Spelling check is on");
+		}
+		else{
+			System.out.println("Spelling check is off");
+		}
+	}	
+}
